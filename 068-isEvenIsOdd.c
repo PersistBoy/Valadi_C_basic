55584f8ee05ae005853c190b90d2cfde58677555
PersistBoy <motlagh567@gmail.com>
2023-04-19 19:47:17 +0330

    069 functions exercise

--- 068-isEvenIsOdd.c
+++ 068-isEvenIsOdd.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int isEven(int num)
+int isEven(int num1)
 {
-    if (num%2==0)
+    if (num1%2==0)
         return 1;
     else 
         return 0;
+}
+
+int isEven(int num2)
+{
+    if (num2%2==0)
+        return 0;
+    else
+        return 1;
 }
 
 int main()
---
+++ 069-the9Magic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int magicOf9(int num)
+{
+    int i,resault;
+    for (i=1 ; i<=num ; i++)
+    {
+        resault=i*9;
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}
