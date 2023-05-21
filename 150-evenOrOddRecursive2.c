c3c0b52913a8af455c6b1cda7adf0d24611a0703
unknown <motlagh567@gmail.com>
2023-05-21 16:16:03 +0430

    recursive exercise

--- 150-evenOrOddRecursive2.c
+++ 150-evenOrOddRecursive2.c
@@ -5,14 +5,17 @@ int evenOrOddRecursive(int num)
     int sum;
     if (num<=9)
     {
-        return num;
+        if (num%2 == 0)
+            return 1;
+        else
+            return 0;
     }
     else
     {
-        if (((num%10)+evenOrOddRecursive(num/10)) % 2 !=0)
-            return 1;
-        else 
+        if (evenOrOddRecursive(num/2) == 1 && )
             return 0;
+        else 
+            return 1;
     }
 }
 
